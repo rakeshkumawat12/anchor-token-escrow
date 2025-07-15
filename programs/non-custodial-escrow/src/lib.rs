86ad7d06ecdce969a13bfe8170c93df5538e1537
rakeshkumawat12 <rakeshkumawatrrkk12877@gmail.com>
2025-07-15 13:04:27 +0530

    Final update

--- programs/non-custodial-escrow/src/lib.rs
+++ programs/non-custodial-escrow/src/lib.rs
@@ -1,58 +1,187 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{transfer, Mint, Transfer, TokenAccount,Token};
+use anchor_spl::token::{transfer, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("24UbSkST9eUGakWTfvj1Z86utWHRcVuwzLJfYgzhBDgQ");
 
 #[program]
 pub mod non_custodial_escrow {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let escrow = &ctx.accounts.escrow;
-        escrow.bump = ctx.bumps.escrow;
+   
+    pub fn initialize(ctx: Context<Initialize>, x_amount: u64, y_amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.bump = ctx.bumps.escrow; 
         escrow.authority = ctx.accounts.seller.key();
-        escrow.escrow_x_token = ctx.accounts.escrow_x_tokens.key();
+        escrow.escrow_x_token = ctx.accounts.escrowed_x_tokens.key();
+        
         escrow.y_amount = y_amount;
         escrow.y_mint = ctx.accounts.y_mint.key();
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.seller_x_token.to_account_info(),
+            to: ctx.accounts.escrowed_x_tokens.to_account_info(),
+            authority: ctx.accounts.seller.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        transfer(cpi_ctx, x_amount)?;
+        Ok(())
+    }
+
+    pub fn accept(ctx: Context<Accept>) -> Result<()> {
+      
+        anchor_spl::token::transfer(
+            CpiContext::new_with_signer(
+             
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                    to: ctx.accounts.buyer_x_token.to_account_info(),
+                    authority: ctx.accounts.escrow.to_account_info(),
+                },
+                &[&[
+                    "escrow".as_bytes(),
+                    ctx.accounts.escrow.authority.as_ref(),
+                    &[ctx.accounts.escrow.bump],
+                ]],
+            ),
+            ctx.accounts.escrowed_x_tokens.amount,
+        )?;
+
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.buyer_y_token.to_account_info(),
+                    to: ctx.accounts.seller_y_token.to_account_info(),
+                    authority: ctx.accounts.buyer.to_account_info(),
+                },
+            ),
+            ctx.accounts.escrow.y_amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+  
+        anchor_spl::token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                    to: ctx.accounts.seller_x_token.to_account_info(),
+                    authority: ctx.accounts.escrow.to_account_info(),
+                },
+                &[&[
+                    "escrow".as_bytes(),
+                    ctx.accounts.escrow.authority.as_ref(),
+                    &[ctx.accounts.escrow.bump],
+                ]],
+            ),
+            ctx.accounts.escrowed_x_tokens.amount,
+        )?;
+        anchor_spl::token::close_account(CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            anchor_spl::token::CloseAccount {
+                account: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                destination: ctx.accounts.seller.to_account_info(),
+                authority: ctx.accounts.escrow.to_account_info(),
+            },
+            &[&[
+                "escrow".as_bytes(),
+                ctx.accounts.seller.key().as_ref(),
+                &[ctx.accounts.escrow.bump],
+            ]],
+        ))?;
         Ok(())
     }
 }
-
-#[derive(Accounts, InitSpace)]
+#[account]
+#[derive(InitSpace)]
 pub struct Escrow {
-    authority: Pubkey,
+    authority: Pubkey, 
     escrow_x_token: Pubkey,
-    bump: u8,
+    bump: u8, 
     y_amount: u64,
-    y_mint: Pubkey,
+    y_mint: Pubkey, 
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
     pub seller: Signer<'info>,
+    pub x_mint: Account<'info, Mint>,
+    pub y_mint: Account<'info, Mint>, 
+    #[account(mut,constraint=seller_x_token.mint==x_mint.key() && seller_x_token.owner==seller.key())]
+    pub seller_x_token: Account<'info, TokenAccount>,
 
-    x_mint: Account<'info, Mint>,
-    y_mint: Account<'info, Mint>,
-
-    #[account(
-        init,
+    #[account(init,
         payer=seller,
         space=8+Escrow::INIT_SPACE,
-        seeds=["escrow".as_bytes(), seller.key().as_ref()],
-        bump
-    )]
-    pub escrow: Account<'info, Escrow>,
+        seeds=["escrow".as_bytes(),seller.key().as_ref()],
+        bump)]
+    pub escrow: Account<'info, Escrow>, 
 
     #[account(
         init,
         payer=seller,
         token::mint=x_mint,
-        token::authority=escrow
-    )]
-    pub escrow_x_tokens: Account<'info, TokenAccount>,
+        token::authority=escrow)] 
+    pub escrowed_x_tokens: Account<'info, TokenAccount>,
 
-    system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>, 
+    pub rent: Sysvar<'info, Rent>,            
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct Accept<'info> {
+    pub buyer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=["escrow".as_bytes(),escrow.authority.as_ref()],
+        bump = escrow.bump, 
+    )]
+    escrow: Account<'info, Escrow>, 
+
+    #[account(mut,constraint=escrowed_x_tokens.key()==escrow.escrow_x_token)]
+
+    pub escrowed_x_tokens: Account<'info, TokenAccount>,
+
+    #[account(mut,constraint=seller_y_token.mint==escrow.y_mint)]
+    pub seller_y_token: Account<'info, TokenAccount>,
+
+    #[account(mut,constraint=buyer_x_token.mint==escrowed_x_tokens.mint)]
+    pub buyer_x_token: Account<'info, TokenAccount>,
+
+    #[account(mut,
+        constraint=buyer_y_token.mint==escrow.y_mint,
+        constraint =  buyer_y_token.owner==buyer.key())]
+    pub buyer_y_token: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>, 
+}
+
+#[derive(Accounts)]
+pub struct Cancel<'info> {
+    pub seller: Signer<'info>,
+ 
+    #[account(
+        mut,
+        close = seller, constraint = escrow.authority == seller.key(),
+        seeds=["escrow".as_bytes(),escrow.authority.as_ref()],
+        bump = escrow.bump,
+    )]
+    escrow: Account<'info, Escrow>, 
+
+    #[account(mut,constraint = escrowed_x_tokens.key()==escrow.escrow_x_token)]
+  
+    pub escrowed_x_tokens: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint=seller_x_token.mint==escrowed_x_tokens.mint,
+        constraint = seller_x_token.owner==seller.key()
+    )]
+    seller_x_token: Account<'info, TokenAccount>,
     token_program: Program<'info, Token>,
-    rent: Sysvar<'info, Rent>
 }
--- tests/non-custodial-escrow.ts
+++ tests/non-custodial-escrow.ts
@@ -1,16 +1,233 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { NonCustodialEscrow } from "../target/types/non_custodial_escrow";
-
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+  getAccount,
+} from "@solana/spl-token";
+import { SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 describe("non-custodial-escrow", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.nonCustodialEscrow as Program<NonCustodialEscrow>;
+  const program = anchor.workspace
+    .nonCustodialEscrow as Program<NonCustodialEscrow>;
+
+  const seller = provider.wallet.publicKey;
+  const buyer = anchor.web3.Keypair.generate();
+  const payer = (provider.wallet as NodeWallet).payer;
+  const escrowedXTokens = anchor.web3.Keypair.generate();
+  let x_mint: anchor.web3.PublicKey;
+  let y_mint: anchor.web3.PublicKey;
+
+  let seller_x_token: anchor.web3.PublicKey;
+  let seller_y_token: anchor.web3.PublicKey;
+  let buyer_x_token: anchor.web3.PublicKey;
+  let buyer_y_token: anchor.web3.PublicKey;
+  let escrow: anchor.web3.PublicKey;
+
+  before(async () => {
+    [escrow] = anchor.web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode("escrow"), seller.toBuffer()],
+      program.programId
+    );
+
+    x_mint = await createMint(provider.connection, payer, seller, null, 6);
+    y_mint = await createMint(provider.connection, payer, seller, null, 6);
+    seller_x_token = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        payer,
+        x_mint,
+        seller
+      )
+    ).address;
+    await mintTo(
+      provider.connection,
+      payer,
+      x_mint,
+      seller_x_token,
+      payer,
+      10_000_000_000
+    );
+
+    seller_y_token = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        payer,
+        y_mint,
+        seller
+      )
+    ).address;
+    await mintTo(
+      provider.connection,
+      payer,
+      y_mint,
+      seller_y_token,
+      payer,
+      10_000_000_000
+    );
+
+    buyer_x_token = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        payer,
+        x_mint,
+        buyer.publicKey
+      )
+    ).address;
+    await mintTo(
+      provider.connection,
+      payer,
+      x_mint,
+      buyer_x_token,
+      payer,
+      10_000_000_000
+    );
+
+    buyer_y_token = (
+      await getOrCreateAssociatedTokenAccount(
+        provider.connection,
+        payer,
+        y_mint,
+        buyer.publicKey
+      )
+    ).address;
+    await mintTo(
+      provider.connection,
+      payer,
+      y_mint,
+      buyer_y_token,
+      payer,
+      10_000_000_000
+    );
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const x_amount = new anchor.BN(100);
+    const y_amount = new anchor.BN(40);
+    const sellerXBefore = (
+      await getAccount(provider.connection, seller_x_token)
+    ).amount;
+
+    const tx = await program.methods
+      .initialize(x_amount, y_amount)
+      .accounts({
+        seller,
+        xMint: x_mint,
+        yMint: y_mint,
+        sellerXToken: seller_x_token,
+        escrow,
+        escrowedXTokens: escrowedXTokens.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([escrowedXTokens])
+      .rpc();
+    const sellerXAfter = (await getAccount(provider.connection, seller_x_token))
+      .amount;
+    const escrowedXAfter = (
+      await getAccount(provider.connection, escrowedXTokens.publicKey)
+    ).amount;
+    console.log(
+      "Seller X Before:",
+      sellerXBefore.toString(),
+      "After:",
+      sellerXAfter.toString()
+    );
+    console.log(
+      "Escrowed X Before: Does not exist",
+      "Escrowed X After:",
+      escrowedXAfter.toString()
+    );
+  });
+
+  it("Is Accepted!", async () => {
+    const sellerXBefore = (
+      await getAccount(provider.connection, seller_x_token)
+    ).amount;
+    const buyerXBefore = (await getAccount(provider.connection, buyer_x_token))
+      .amount;
+    const sellerYBefore = (
+      await getAccount(provider.connection, seller_y_token)
+    ).amount;
+    const buyerYBefore = (await getAccount(provider.connection, buyer_y_token))
+      .amount;
+    const tx = await program.methods
+      .accept()
+      .accounts({
+        buyer: buyer.publicKey,
+        sellerYToken: seller_y_token,
+        buyerXToken: buyer_x_token,
+        buyerYToken: buyer_y_token,
+        escrow,
+        escrowedXTokens: escrowedXTokens.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([buyer])
+      .rpc();
+    const sellerXAfter = (await getAccount(provider.connection, seller_x_token))
+      .amount;
+    const buyerXAfter = (await getAccount(provider.connection, buyer_x_token))
+      .amount;
+    const sellerYAfter = (await getAccount(provider.connection, seller_y_token))
+      .amount;
+    const buyerYAfter = (await getAccount(provider.connection, buyer_y_token))
+      .amount;
+    const escrowedXAfter = (
+      await getAccount(provider.connection, escrowedXTokens.publicKey)
+    ).amount;
+    console.log(
+      "Seller X Before:",
+      sellerXBefore.toString(),
+      "After:",
+      sellerXAfter.toString()
+    );
+    console.log(
+      "Buyer X Before:",
+      buyerXBefore.toString(),
+      "After:",
+      buyerXAfter.toString()
+    );
+    console.log(
+      "Seller Y Before:",
+      sellerYBefore.toString(),
+      "After:",
+      sellerYAfter.toString()
+    );
+    console.log(
+      "Buyer Y Before:",
+      buyerYAfter.toString(),
+      "After:",
+      buyerYAfter.toString()
+    );
+    console.log(
+      "Escrowed X Before: Does not exist",
+      "Escrowed X After:",
+      escrowedXAfter.toString()
+    );
+  });
+
+  it("Is Cancel!", async () => {
+    const sellerXBefore = (
+      await getAccount(provider.connection, seller_x_token)
+    ).amount;
+
+    const tx = await program.methods
+      .cancel()
+      .accounts({
+        seller,
+        escrow,
+        sellerXToken: seller_x_token,
+        escrowedXTokens: escrowedXTokens.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .rpc();
   });
 });
